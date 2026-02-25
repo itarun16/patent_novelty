60c4219b1ab02d64955377dbda6841d08bda851b
itarun16 <123171516+itarun16@users.noreply.github.com>
2026-02-25 12:41:34 +0530

    add frontend requirements

--- frontend/app.py
+++ frontend/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import fitz
 
-API_URL = "http://127.0.0.1:8000/search"
+API_URL = "https://patentnovelty-production.up.railway.app/search"
 
 
 def extract_text(upload):
